95da910731553abc729c26162a09b27bacddb8ce
Patrick Sagum <sagum.patrick.r@gmail.com>
2025-01-05 16:03:59 +0800

    Admin Changed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Run in your command prompt
+npm install
+
+# Compiles CSS files using Tailwind CSS
+npm run build:css
---
+++ admin/src/assets/js/chart.js
@@ -0,0 +1,358 @@
+const customerSatisfaction = () => {
+  return {
+    series: [92, 8],
+    colors: ["#FC8002", "gray",],
+    chart: {
+      height: "280px",
+      width: "100%",
+      type: "pie",
+    },
+    stroke: {
+      colors: ["white"],
+      lineCap: "",
+    },
+    plotOptions: {
+      pie: {
+        labels: {
+          show: true,
+        },
+        size: "90%",
+        dataLabels: {
+          offset: -25
+        }
+      },
+    },
+    labels: ["Customer Satisfaction", ""],
+    dataLabels: {
+      enabled: true,
+      formatter: function (val, opts) {
+        if (val === 92) {
+          return val + "%";
+        } else {
+          return "";
+        }
+      },
+      style: {
+        fontFamily: "Poppins, sans-serif",
+      },
+    },
+    legend: {
+      position: "bottom",
+      fontFamily: "Poppins, sans-serif",
+    },
+    yaxis: {
+      labels: {
+        formatter: function (value) {
+          return value + "%";
+        },
+      },
+    },
+    xaxis: {
+      labels: {
+        formatter: function (value) {
+          return value + "%";
+        },
+      },
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+    },
+  };
+};
+
+if (document.getElementById("pie-chart") && typeof ApexCharts !== 'undefined') {
+  const chart = new ApexCharts(document.getElementById("pie-chart"), customerSatisfaction());
+  chart.render();
+}
+
+
+// FLEET UTILIZATION RATE CHARTS
+
+const fleetUtilization = {
+  // set the labels option to true to show the labels on the X and Y axis
+  xaxis: {
+    show: true,
+    categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan'],
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "Poppins, sans-serif",
+        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+      }
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    show: true,
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "Poppins, sans-serif",
+        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+      },
+      formatter: function (value) {
+        return value;
+      }
+    }
+  },
+  series: [
+    {
+      name: "Fleet Utilizatio Rate",
+      data: [0, 50, 100, 3, 2, 1, 1],
+      color: "#FC8002",
+    },
+    // {
+    //   name: "Designer Edition",
+    //   data: [43, 13, 65, 12, 42, 73],
+    //   color: "#7E3BF2",
+    // },
+  ],
+  chart: {
+    sparkline: {
+      enabled: false
+    },
+    height: "290px",
+    width: "100%",
+    type: "area",
+    fontFamily: "Poppins, sans-serif",
+    dropShadow: {
+      enabled: false,
+    },
+    toolbar: {
+      show: false,
+    },
+  },
+  tooltip: {
+    enabled: true,
+    x: {
+      show: false,
+    },
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      opacityFrom: 0.55,
+      opacityTo: 0,
+      shade: "#1C64F2",
+      gradientToColors: ["#1C64F2"],
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    width: 6,
+  },
+  legend: {
+    show: false
+  },
+  grid: {
+    show: true,
+  },
+  }
+  
+  if (document.getElementById("labels-chart") && typeof ApexCharts !== 'undefined') {
+  const chart = new ApexCharts(document.getElementById("labels-chart"), fleetUtilization);
+  chart.render();
+  }
+
+
+
+// Delivery on time
+const deliveryOnTime = {
+  colors: ["#1A56DB", "#FDBA8C"],
+  series: [
+    {
+      name: "Rate",
+      color: "#FC8002",
+      data: [
+        { x: "Downtown", y: 85 },
+        { x: "Sub urban", y: 81 },
+        { x: "Rural", y: 89 },
+        { x: "Coastal", y: 90 },
+      ],
+    },
+  ],
+  chart: {
+    type: "bar",
+    height: "285px",
+    fontFamily: "Poppins, sans-serif",
+    toolbar: {
+      show: false,
+    },
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: "60%",
+      borderRadiusApplication: "end",
+      borderRadius: 8,
+    },
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    style: {
+      fontFamily: "Poppins, sans-serif",
+    },
+  },
+  states: {
+    hover: {
+      filter: {
+        type: "darken",
+        value: 1,
+      },
+    },
+  },
+  stroke: {
+    show: true,
+    width: 0,
+    colors: ["transparent"],
+  },
+  grid: {
+    show: true,
+    strokeDashArray: 4,
+    padding: {
+      left: 2,
+      right: 2,
+      top: -14
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    floating: false,
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "Poppins, sans-serif",
+        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+      }
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    show: true,
+  },
+  fill: {
+    opacity: 1,
+  },
+}
+
+if(document.getElementById("column-chart") && typeof ApexCharts !== 'undefined') {
+  const chart = new ApexCharts(document.getElementById("column-chart"), deliveryOnTime);
+  chart.render();
+}
+
+// AVERAGE DELIVERY TIME
+
+const aveDelTime = {
+  series: [
+    {
+      name: "Delivery Time (minutes)",
+      color: "#FC8002",
+      data: ["50", "43", "54", "40",],
+    }
+  ],
+  chart: {
+    sparkline: {
+      enabled: false,
+    },
+    type: "bar",
+    width: "100%",
+    height: 215,
+    toolbar: {
+      show: false,
+    }
+  },
+  fill: {
+    opacity: 1,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      columnWidth: "70%",
+      borderRadiusApplication: "end",
+      borderRadius: 6,
+      dataLabels: {
+        position: "top",
+      },
+    },
+  },
+  legend: {
+    show: true,
+    position: "bottom",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    formatter: function (value) {
+      return value
+    }
+  },
+  xaxis: {
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "Poppins, sans-serif",
+        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+      },
+      formatter: function(value) {
+        return value
+      }
+    },
+    categories: ["Downtown", "Suburban", "Rural", "Coastal"],
+    axisTicks: {
+      show: false,
+    },
+    axisBorder: {
+      show: false,
+    },
+  },
+  yaxis: {
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "Poppins, sans-serif",
+        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+      }
+    }
+  },
+  grid: {
+    show: true,
+    strokeDashArray: 4,
+    padding: {
+      left: 2,
+      right: 2,
+      top: -20
+    },
+  },
+  fill: {
+    opacity: 1,
+  }
+}
+
+if(document.getElementById("bar-chart") && typeof ApexCharts !== 'undefined') {
+  const chart = new ApexCharts(document.getElementById("bar-chart"), aveDelTime);
+  chart.render();
+}
